fe07b223042afe6e4fc792d3bb33f98ae8ad9901
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-09-03 21:28:02 +0100

    Created Gallery Page

--- client/src/components/blocks/Header.tsx
+++ client/src/components/blocks/Header.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-const Header = (
+type Header = {
     pageName: string, 
     tagLine: string, 
     description: string
+}
+
+const Header = (
+    {pageName, tagLine, description}: Header
 ) => {
     return (
         <section className="page-hero">
             <div className="wrap">
                 <p className="eyebrow">{pageName}</p>
-                <h1>M{tagLine}</h1>
+                <h1>{tagLine}</h1>
                 <p>{description}</p>
             </div>
         </section>
